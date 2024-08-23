b0fc5ab32b0b7a69a6ef69aaef96562d64a8860b
Gue-ho <iamleohwang@gmail.com>
2024-08-23 00:46:22 -0400

    20240823 merge script
    merging REGULAR and UMI pipeline and upload small data set to test running

---
+++ CRISPRlungo.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+import argparse, sys
+import time, os
+from subprocess import Popen, PIPE
+
+
+# In[23]:
+
+
+import statistical_integration_clean as regular_py
+import Post_process_final as visual
+import CRISPRlungo_umi
+
+
+# In[25]:
+
+
+
+
+
+# In[ ]:
+
+
+def main():
+
+    start_time = time.time()
+
+    if len(sys.argv) == 1:
+        print('usage: CRISPRlungo [regular | umi]')
+        sys.exit()
+
+    parser = argparse.ArgumentParser(description='Analysis CRISPR mutaion pattern for Long-read sequencing')
+    subparsers = parser.add_subparsers(dest='command')
+
+    regular_parser = subparsers.add_parser('regular', help='Analyze CRISPR mutation for Long-read sequencing data without UMI')
+    regular_parser.add_argument('ref', type=str,  help='Reference FASTA file')
+    regular_parser.add_argument('control', type=str,  help='Control FASTQ file of Long-read sequencing data')
+    regular_parser.add_argument('treated', type=str,  help='Treated FASTQ file of Long-read sequencing data')
+    regular_parser.add_argument('output', type=str, help='Output file name')
+    regular_parser.add_argument('-t', '--threads', type=int, default=1, help='Output file name')
+    regular_parser.add_argument('--target', type=str, default=None, help='Target sequence')
+
+    umi_parser = subparsers.add_parser('umi', help='Analyze CRISPR mutation for Long-read sequencing data with UMI')
+    umi_parser.add_argument('ref', type=str,  help='Reference FASTA file')
+    umi_parser.add_argument('input_file', type=str,  help='FASTQ file of Long-read sequencing data')
+    umi_parser.add_argument('output_dir', type=str,  help='Directory for output files')
+    umi_parser.add_argument('target', type=str,  help='Target sequence without PAM')
+    umi_parser.add_argument('-t', '--threads', type=int, default=1, help='Output file name')
+    umi_parser.add_argument('-c', '--clust_cutoff', type=int, default=5, help='The minimum of UMI cluster size')
+    umi_parser.add_argument('--cleavage_pos', type=int, default=16, help='Cleavage position in target sequence')
+    umi_parser.add_argument('--window', type=int, default=10, help='The range for mutation analysis around cleavage site')
+
+    
+    args = parser.parse_args()
+
+
+    def run_minimap2(ref_file, input_file, output_file, longjoin_bandwidth, chaining_bandwidth, threads):
+        align_st_time = time.time()
+        print(f'minimap2 aligning {input_file} ...\r', end='')
+        p = Popen(f'minimap2 -ax map-ont -t {threads} -r {chaining_bandwidth},{longjoin_bandwidth} {ref_file} {input_file} -o {output_file}', shell=True, stderr=PIPE, stdout=PIPE).communicate()
+        if p[1].decode('utf-8').find('ERROR') != -1:
+            print(p[1].decode('utf-8'))
+            sys.exit()
+        else:
+            print(f'minimap2 aligning {input_file} ... Done {time.time() - align_st_time} s')
+        
+    def create_dir(dir_name):
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+    
+    if args.command == 'regular':
+        
+        print('Start to analysis using REGULAR mode ...')
+
+               
+        output_dir = os.path.dirname(args.output)
+        if output_dir == '':
+            output_dir = './'
+        threads = args.threads
+        
+        # Run Miniseq2 to align received files
+        ref_file = args.ref
+        ref_seq = ''.join(open(ref_file).readlines()[1:]).replace('\n','').upper()
+        ref_len = len(ref_seq)
+        if args.target != None:
+            target = args.target.upper()
+            if ref_seq.find(target) != -1:
+                cv_pos = ref_seq.find(target) + 16
+            elif ref_seq.find(target.translate(target.maketrans('ATGC','TACG'))[::-1]) != -1:
+                cv_pos = ref_seq.find(target.translate(target.maketrans('ATGC','TACG'))[::-1]) + 3
+        longjoin_bandwidth = int(ref_len * 0.3)
+        if longjoin_bandwidth < 500:
+            chaining_bandwidth = longjoin_bandwidth
+        else:
+            chaining_bandwidth = 500
+
+        ref_indexed_file = os.path.splitext(ref_file)[0] + '.mmi'
+        Popen(f'minimap2 -d {ref_indexed_file} {ref_file}', shell=True, stderr=PIPE, stdout=PIPE).communicate()
+
+        run_minimap2(ref_indexed_file, args.control,  output_dir + '/Control_alignment.sam', longjoin_bandwidth, chaining_bandwidth, threads)
+        run_minimap2(ref_indexed_file, args.treated,  output_dir + '/Treated_alignment.sam', longjoin_bandwidth, chaining_bandwidth, threads)
+
+
+        # Run Statistical anlaysis.py
+
+        edited_dictionary, controll_dictionary, List_of_valid_IDs = regular_py.analysis_function(output_dir + '/Control_alignment.sam', output_dir + '/Treated_alignment.sam', ref_file)
+        regular_py.process_mutations(edited_dictionary, args.output + '.tsv', List_of_valid_IDs)
+
+
+    elif args.command == 'umi':
+
+        print('Start to analyze using UMI mode ...')
+        
+        ref_file = args.ref
+        output_dir = args.output_dir
+        input_file = args.input_file
+        threads = args.threads
+
+        # Get UMI position information
+        umi_pos, umi_len, ref_name, ref_seq = CRISPRlungo_umi.input_organize(ref_file, output_dir,input_file)
+        ref_len = len(ref_seq)
+
+        # Make output folders
+        create_dir(output_dir)
+    
+        ref_dir = f'{output_dir}/ref_seq'
+        create_dir(ref_dir)
+        
+        with open(ref_dir + '/ref.fasta', 'w') as f:
+        	f.write(f'>{ref_name}\n{ref_seq}')
+        
+        create_dir(output_dir + '/align')
+
+        # Align recieved FASTQ file
+        longjoin_bandwidth = int(ref_len * 0.3)
+        if longjoin_bandwidth < 500:
+            chaining_bandwidth = longjoin_bandwidth
+        else:
+            chaining_bandwidth = 500
+            
+        p = Popen(f'minimap2 -d {ref_dir}/ref.mmi {ref_dir}/ref.fasta', shell=True, stderr=PIPE, stdout=PIPE).communicate()
+        run_minimap2(ref_dir + "/ref.mmi", args.input_file,  output_dir + '/align/fastq.sam', longjoin_bandwidth, chaining_bandwidth, threads)
+
+        # Get UMI sequence
+        create_dir(f'{output_dir}/demultiplexing')
+
+        CRISPRlungo_umi.extract_index_umi(output_dir + '/align/fastq.sam', ref_file, output_dir, umi_pos[0], umi_pos[1])
+
+        # Clustering UMI sequence
+
+        create_dir(f'{output_dir}/clustering')
+        create_dir(f'{output_dir}/consensus')
+
+        index_info = 'result,NNNNNNNNNN' #For index demultiplexing, not yet supported.
+
+        index_info_list = index_info.split(',')
+        index_names = []
+
+        for i in range(int(len(index_info_list)/2)):
+            
+            i = index_info_list[2*i]
+            index_names.append(i)
+            
+            create_dir(f'{output_dir}/clustering/{i}')
+            create_dir(f'{output_dir}/clustering/{i}/1st_clusters')
+            create_dir(f'{output_dir}/clustering/{i}/2nd_clusters')
+            create_dir(f'{output_dir}/clustering/{i}/medaka_input')
+            create_dir(f'{output_dir}/consensus/{i}')
+            
+        create_dir(f'{output_dir}/results')
+
+        for fn in index_names:
+            
+            CRISPRlungo_umi.clustering_umi( f'{output_dir}/demultiplexing/umi_{fn}.fasta', umi_len, f'{output_dir}/clustering/{fn}', f'{output_dir}/consensus/{fn}', args.clust_cutoff, threads)
+
+            run_minimap2(ref_dir + "/ref.mmi", f'{output_dir}/consensus/{fn}/consensus.fasta', f'{output_dir}/align/consensus_result.sam', longjoin_bandwidth, chaining_bandwidth, threads)
+
+            # Mutation Analysis
+            print('Mutation analysis...')
+            create_dir(f'{output_dir}/results/{fn}')
+            cv_pos = CRISPRlungo_umi.mutation_analysis(ref_seq, args.target.upper(), args.cleavage_pos, args.window, f'{output_dir}/align/consensus_{fn}.sam', f'{output_dir}/results/{fn}', threads=threads)
+
+    #visualization
+
+    read_per_position = visual.visualization_preprocess(output_dir + '/Treated_alignment.sam', ref_file)
+    if args.command == 'regular':
+        visual.regular_statistic_plot(args.output + '.tsv', args.treated, output_dir)
+        visual.regular_accuracy_plot(ref_seq, read_per_position, output_dir)
+
+        tsv_file = args.output + '.tsv'
+
+    else:
+        tsv_file = output_dir + '/results/result/read_classification.txt'
+
+    visual.base_proportion(read_per_position, output_dir, ref_seq)
+    visual.mutation_pie_chart(tsv_file, output_dir)
+    visual.indel_per_position(tsv_file, ref_seq, output_dir)
+    visual.Insertion_length(tsv_file, output_dir)
+    visual.Deletion_length(tsv_file, output_dir)
+    visual.Deletion_count_length(tsv_file, output_dir)
+    if args.target != None:
+        visual.allele_plot(ref_seq, cv_pos, tsv_file, output_dir)
+    #LD_tornado()
+      
+
+        
+
+
+# In[ ]:
+
+
+if __name__=='__main__':
+    main()
+
---
+++ statistical_integration_clean.py
@@ -0,0 +1,366 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pysam
+from scipy.stats import fisher_exact
+from scipy.stats import chi2_contingency
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import pysam
+import collections
+from Bio import SeqIO
+import sys
+
+def analysis_function(control,edited,refernce, p_limit_value = 0.002,allowance_value = 0.05, pooling = True, title = "90"):
+    # Path to SAM files
+    samfile_path1 = edited
+    samfile_path2 = control
+
+    
+    fasta_file = refernce
+
+    def find_sub(read,fasta_file):
+        reference_sequence = ""
+        for record in SeqIO.parse(fasta_file, "fasta"):
+            reference_sequence = str(record.seq)
+            break
+
+        # Open the SAM file
+        substitution_list = []
+       
+        read_seq = read.query_sequence
+        
+        # Iterate over the aligned portion of the read
+        for read_idx, ref_idx in enumerate(read.get_reference_positions(full_length=True)):
+            if ref_idx is None:
+                continue
+            read_base = read_seq[(read_idx)]
+            if read_base != reference_sequence[ref_idx]:
+                substitution_list.append(("Sub_"+str(reference_sequence[ref_idx])+">"+str(read_base),ref_idx,1))
+
+        # Output the counts for each position
+        return(substitution_list)
+
+    def combine_deletions(tuples):
+        # Function to check if two deletions should be combined
+        if len(tuples)<=1:return(tuples)
+
+        def should_combine(pos1, len1, pos2,len2):
+            return (abs(pos2 - (pos1 + len1)) <= 0.05 * (len1+len2)/2 and len1>=5 and len2>=5)
+
+        how_many_pop =0
+        combined_tuples = []
+        for i in range(0,len(tuples)):
+             tuple1 = tuples[i-how_many_pop]
+             if list(tuple1)[2]<=5: 
+                  tuples.pop(i-how_many_pop)
+                  how_many_pop+=1
+                  combined_tuples.append(tuple1)
+        i = 0
+        while i < len(tuples)-1:
+            if tuples[i][0] == "insertion" or tuples[i][0] == "deletion" and tuples[i+1][0] == tuples[i][0]:
+                pos1, len1 = tuples[i][1], tuples[i][2]
+                j = i + 1
+                while j < (len(tuples)-1) and tuples[j][0] == tuples[i][0] and should_combine(pos1, len1, tuples[j][1], tuples[j][2]):
+                    len1 += tuples[j][2] + (tuples[j][1] - (pos1 + len1))
+                    j += 1
+                combined_tuples.append((tuples[j][0], pos1, len1))
+                i = j
+            else:
+                combined_tuples.append(tuples[i])
+                i += 1
+        return combined_tuples
+
+    def quant_unique_indels(samfile_path):
+        # Open the SAM file
+        samfile = pysam.AlignmentFile(samfile_path, 'r')
+        reference_sequence = ""
+        for record in SeqIO.parse(fasta_file, "fasta"):
+            reference_sequence = str(record.seq)
+        # Initialize a dictionary to hold mutation information
+        # Key: (type_of_mutation, position_of_mutation, length_of_mutation)
+        # Value: count of occurrences
+        mutations = {}
+        #initilize dict where read number is key and value is list of mutations
+        total_reads = 0
+        counter = 0
+        # Iterate over reads in the SAM file
+        for read in samfile.fetch():
+            mutations_in_read = []
+            ref_pos = read.reference_start
+            # Skip unmapped reads
+            if read.is_unmapped:
+                continue
+            
+            # Current position in the reference sequence
+            
+            if read.reference_start>100: continue
+            if read.reference_end<(len(reference_sequence)-100): continue
+            
+            if read.mapping_quality <= 30:
+                continue
+    
+            
+            total_reads+=1
+            # Iterate over CIGAR operations
+            for operation, length in read.cigar:
+                # Check for insertion (I) or deletion (D)
+                if operation == 1:  # Insertion
+                    key = ('insertion', ref_pos, length)
+                    mutations_in_read.append(key)
+                elif operation == 2 :  # Deletion
+                    key = ('deletion', ref_pos, length)
+                    mutations_in_read.append(key)
+                # Update ref_pos based on operation
+                if operation in [0, 2, 3]:  # Match/Mismatch, Deletion, N (Skipped region)
+                    ref_pos += length
+
+                counter +=1
+            #combined_mutations_in_read = combine_deletions(mutations_in_read)
+            #if len(combined_mutations_in_read) != len(mutations_in_read):
+               # combined_mutations_in_read = combine_deletions(combined_mutations_in_read)
+            
+            #Substitution counter
+
+            subs_in_read = find_sub(read,fasta_file)
+            for sub in subs_in_read: mutations_in_read.append(sub)
+            for mut in mutations_in_read:
+                mutations[mut] = mutations.get(mut, 0) + 1
+
+            
+        # Close the SAM file
+        samfile.close()
+        return(mutations,total_reads)
+
+
+
+    def custom_round(num,margin):
+        if math.floor(num*margin)  == 0: return(num)
+        rond = round(num * margin)/margin
+        fit = round(num * margin)
+        closest_value =  rond + round((num-rond)/fit)*fit
+        return int(closest_value)
+
+    def pool(mut_dict, allowance = allowance_value):
+        outputdict = {}
+        for key in mut_dict.keys():
+            if math.floor(list(key)[2]*allowance)  == 0: outputdict[key] = mut_dict[key]
+            else: 
+                newlength = custom_round(list(key)[2],allowance)
+                newpos = custom_round(list(key)[1],allowance)
+                type = list(key)[0]
+                mutation = (type,newpos,newlength)
+                outputdict[mutation] = outputdict.get(mutation, 0) + mut_dict[key]
+        return(outputdict)
+
+    def significant_mutations(control_dict, edited_dict, control_reads, edited_reads, p_limit = p_limit_value):
+        """
+        Performs a Fisher's Exact Test on each mutation to compare its frequency between
+        a control and an edited dataset. Returns the keys of the significant mutations.
+
+        Parameters:
+        - control_dict: Dictionary of mutations for the control dataset.
+        - edited_dict: Dictionary of mutations for the edited dataset.
+        - control_reads: Total number of reads in the control dataset.
+        - edited_reads: Total number of reads in the edited dataset.
+
+        Returns:
+        - A list of keys for the mutations that are significantly different.
+        """
+        
+
+        significant_keys = []
+        pvalues = []
+        count_print = 0
+        # Combine all keys from both dictionaries
+        all_keys = set(control_dict.keys()) | set(edited_dict.keys())
+        
+        for key in all_keys:
+            # Get the counts for this mutation in both datasets
+            control_count = control_dict.get(key, 0)
+            edited_count = edited_dict.get(key, 0)
+            length = list(key)[2]
+            if control_count/control_reads > edited_count/edited_reads:
+                continue
+        
+            # Create the contingency table for this mutation
+            table = [
+                [control_count, control_reads - control_count],  # Control counts
+                [edited_count, edited_reads - edited_count]      # Edited counts
+            ]
+            
+            
+            # Perform Fisher's Exact Test if control count is less than  or equal to 5 or chi2 if control_count >5
+            if control_count <= 5:
+                _, p_value = fisher_exact(table, alternative='two-sided')
+            if control_count > 5:
+                chi2, p_value, dof, expected = chi2_contingency(table)
+            
+        
+            
+            # Check for significance 
+            if p_value<=p_limit:
+                    significant_keys.append(key)
+                    pvalues.append(p_value)
+            
+        return significant_keys,pvalues
+
+
+    # Separate insertion and deletion counts for histogram plotting
+     
+
+    def creat_dict_analysis(samfile_path, significant_keys):
+        # Open the SAM file
+        reference_sequence = ""
+        for record in SeqIO.parse(fasta_file, "fasta"):
+            reference_sequence = str(record.seq)
+        samfile = pysam.AlignmentFile(samfile_path, 'r')
+
+        # Initialize a dictionary to hold mutation information
+        # Key: (type_of_mutation, position_of_mutation, length_of_mutation)
+        # Value: count of occurrences
+        mutations = {}
+        #initilize dict where read number is key and value is list of mutations
+        dict_of_reads = {}
+        total_reads = 0
+        set_sig_mut = set(significant_keys)
+        # Iterate over reads in the SAM file
+        reads_that_passed = []
+        for read in samfile.fetch():
+            mutations_in_read = []
+            # Skip unmapped reads
+            if read.is_unmapped:
+                continue
+            #create more varible way to quantify end
+            if read.reference_start>100 or read.reference_end<(len(reference_sequence)-100):
+                continue
+
+            if read.mapping_quality <= 30:
+                continue
+            reads_that_passed.append(str(read).split('	',1)[0])
+
+            total_reads+=1
+            #if total_reads%1000 == 0: print(read.reference_start)
+            dict_of_reads[total_reads] = []
+            # Current position in the reference sequence
+            ref_pos = read.reference_start
+            
+            # Iterate over CIGAR operations
+            for operation, length in read.cigar:
+                # Check for insertion (I) or deletion (D)
+                
+                if operation == 1:  # Insertion
+                    key = ('insertion', ref_pos, length)
+
+                elif operation == 2:  # Deletion
+                    key = ('deletion', ref_pos, length)
+
+                if operation in [0, 2, 3]:  # Match/Mismatch, Deletion, N (Skipped region)
+                    ref_pos += length
+
+                if operation not in [1,2]: continue
+
+                mutations_in_read.append(key)
+                
+            subs_in_read = find_sub(read,fasta_file)
+            for sub in subs_in_read: mutations_in_read.append(sub)
+            
+            #combined_mutations_in_read = combine_deletions(mutations_in_read)
+            #if len(combined_mutations_in_read) != mutations_in_read: 
+                #combined_mutations_in_read = combine_deletions(combined_mutations_in_read)
+            for mut in mutations_in_read:
+                allowance = allowance_value
+                length1 = list(mut)[2]
+                pos = list(mut)[1]
+                type = list(mut)[0]
+                if pooling:
+                    if (math.floor(length1*allowance)  == 0 or math.floor(list(mut)[1]*allowance)==0) :
+                        pos = list(mut)[1]
+                        type = list(mut)[0]
+                    else:
+                        length1 = custom_round(length1,allowance)
+                        pos = custom_round(list(mut)[1],allowance)
+                        type = list(mut)[0]
+                
+                mutation = (type,pos,length1)
+
+                if mutation in set_sig_mut: dict_of_reads[total_reads].append(mut)
+                # Update ref_pos based on operation
+            
+                
+        # Close the SAM file
+        samfile.close()
+        return(dict_of_reads,reads_that_passed)
+    
+    edited1,readedited1 = quant_unique_indels(samfile_path1)
+    control1,readcontrol1 = quant_unique_indels(samfile_path2)
+
+    if pooling: 
+        control1 = pool(control1)
+        edited1 = pool(edited1)
+
+    significant_keys, pvalues = significant_mutations(control1, edited1, readcontrol1, readedited1)
+    
+    edited_dict_reads, reads_that_passed = creat_dict_analysis(samfile_path1,significant_keys)
+    edited_dict_reads2, _ = creat_dict_analysis(samfile_path2,significant_keys)
+    return(edited_dict_reads,edited_dict_reads2,reads_that_passed)
+    
+    
+
+
+# In[3]:
+
+
+import csv
+
+def classify_mutations(mutations):
+    if not mutations:
+        return 'WT', 'None'
+    
+    def qualifier(type,length):
+        if type == 'deletion':
+            if length < 100:
+                return 'Del'
+            else:
+                return 'Large_Del'
+        elif type == 'insertion':
+            if length < 20:
+                return 'Ins'
+            else:
+                return 'Large_Ins'
+        else: return(type)
+    
+    #2253:Sub_C>G {position}:Sub_{Ref_nt}>{Mut_nt}
+
+    if len(mutations) > 1:
+        return 'Complex', ','.join(f"{pos}_{pos+length-1}:{qualifier(type,length)}_{length}" for type, pos, length in mutations)
+    
+    type, pos, length = mutations[0]
+    if type == 'deletion':
+        if length < 100:
+            return 'Del', f"{pos}_{pos+length-1}:Del_{length}"
+        else:
+            return 'Large_Del', f"{pos}_{pos+length-1}:Large_Del_{length}"
+    elif type == 'insertion':
+        if length < 20:
+            return 'Ins', f"{pos}_{pos+length-1}:Ins_{length}"
+        else:
+            return 'Large_Ins', f"{pos}_{pos+length-1}:Large_Ins_{length}"
+    else: return "Sub", f"{pos}_{pos+length-1}:{type}_{length}"
+    
+
+def process_mutations(mutations_dict, output_file,ids):
+    with open(output_file, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter='\t')
+        writer.writerow(['Read_id', 'Classification', 'Mutation_info'])
+        
+        for read_id, mutations in mutations_dict.items():
+            read_id = ids[read_id-1]
+            
+            classification, mutation_info = classify_mutations(mutations)
+            writer.writerow([read_id, classification, mutation_info])
+
